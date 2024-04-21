876a062c250a279b53528b5c12d21ad4aae228a5
ndevreeze <nico@ndv.nl>
2024-04-21 21:47:26 +0200

    Add most used libraries, most recent version
    Using deps.edn files as input, running lein ancient upgrade.

--- project.clj
+++ project.clj
@@ -4,9 +4,33 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.2"]
-                 [clojure.java-time/clojure.java-time "1.4.2"]   ; 2024-04-03: wrt time/interval in missed-sales.
-                 [org.threeten/threeten-extra "1.7.2"]           ; 2024-04-03: prb this needs to be close to java-time
-                 ]
+                 [org.clojure/tools.cli "1.1.230"]
+                 [org.clojure/data.xml "0.0.8"]
+                 [org.clojure/data.csv "1.1.0"]
+                 [clj-commons/conch "0.9.2"]
+                 [clj-commons/fs "1.6.311"]
+                 [commons-codec "1.16.1"]
+                 [hiccup/hiccup "2.0.0-RC3"]
+                 [nrepl "1.1.1"]
+
+                 ;; 2024-04-21: make explicit because of confusing
+                 ;; deps. Check periodically if it can be removed.
+                 [org.clojure/tools.reader "1.4.0"]
+
+                 ;; 2024-04-03: wrt time/interval, also needs threeten.
+                 [clojure.java-time/clojure.java-time "1.4.2"]
+                 [org.threeten/threeten-extra "1.8.0"]
+
+                 ;; own libraries
+                 [ndevreeze/cmdline "0.2.0"]
+                 [ndevreeze/excel "0.3.2-SNAPSHOT"]
+                 [ndevreeze/flexdb "0.4.2-SNAPSHOT"]
+                 [ndevreeze/logger "0.6.2"]
+
+                 ;; some older libraries to be moved and made public.
+                 [nl.nicodevreeze/org-log-db "0.3.2-SNAPSHOT"]
+                 [nl.nicodevreeze/unison-log "0.2.0-SNAPSHOT"]
+                 [nl.nicodevreeze/xml-db "0.3.0-SNAPSHOT"]]
 
   :target-path "target/%s"
 
@@ -23,4 +47,3 @@
 
   :repositories [["releases" {:url "https://clojars.org/repo/"
                               :creds :gpg}]])
-  
