1b01bfd05193c4cd03ea328a9ebd6a51da1d0638
ndevreeze <nico@ndv.nl>
2026-05-22 22:18:34 +0200

    Bump clj versions for starter on 2026-05-22

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://github.com/ndevreeze/starter"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.12.4"]
+  :dependencies [[org.clojure/clojure "1.12.5"]
                  [org.clojure/tools.cli "1.4.256"]
                  [org.clojure/data.xml "0.0.8"]
                  [org.clojure/data.csv "1.1.1"]
                  [clj-commons/conch "0.9.2"]
                  [clj-commons/fs "1.6.312"]
-                 [commons-codec "1.21.0"]
+                 [commons-codec "1.22.0"]
                  [hiccup/hiccup "2.0.0"]
                  [nrepl "1.7.0"]
 
